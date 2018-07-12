24a2105d88fa0c2e9b15667fc13030f78e198454
Nelson Luz <nelsonluz@cds.eb.mil.br>
2018-07-12 16:44:17 -0300

    Atividades realizadas no dia 12jul2018.

--- app/src/main/java/com/example/nelsonluz/financask/ui/ResumoView.kt
+++ app/src/main/java/com/example/nelsonluz/financask/ui/ResumoView.kt
@@ -1,35 +1,64 @@
 package com.example.nelsonluz.financask.ui
 
+import android.content.Context
+import android.support.v4.content.ContextCompat
 import android.view.View
+import com.example.nelsonluz.financask.R
 import com.example.nelsonluz.financask.extension.formataParaBrasileiro
+import com.example.nelsonluz.financask.model.Resumo
 import com.example.nelsonluz.financask.model.Tipo
 import com.example.nelsonluz.financask.model.Transacao
 import kotlinx.android.synthetic.main.resumo_card.view.*
 import java.math.BigDecimal
 
-class ResumoView(private val view: View,
-                 private val transacoes: List<Transacao>) {
+class ResumoView(private val context: Context,
+                 private val view: View,
+                 transacoes: List<Transacao>) {
 
-    fun adicionaReceita() {
-        var totalReceita = BigDecimal.ZERO
-        for (transacao in transacoes) {
-            if (transacao.tipo == Tipo.RECEITA) {
-                totalReceita = totalReceita.plus(transacao.valor)
-            }
+    private val resumo: Resumo = Resumo(transacoes)
+    private val corReceita = ContextCompat.getColor(context, R.color.receita)
+    private val corDespesa = ContextCompat.getColor(context, R.color.despesa)
 
-        }
-        view.resumo_card_receita.text = totalReceita.formataParaBrasileiro()
+    fun atualiza(){
+        adicionaReceita()
+        adicionaDespesa()
+        adcionaTotal()
     }
 
-     fun adicionaDespesa() {
-        var totalDespesas = BigDecimal.ZERO
-        for (transacao in transacoes) {
-            if (transacao.tipo == Tipo.DESPESA) {
-                totalDespesas = totalDespesas.plus(transacao.valor)
-            }
+    private fun adicionaReceita() {
 
+        val totalReceita = resumo.receita
+        with(view.resumo_card_receita){
+            setTextColor(corReceita)
+            text = totalReceita.formataParaBrasileiro()
         }
-        view.resumo_card_despesa.text = totalDespesas.formataParaBrasileiro()
+    }
+
+
+    private fun adicionaDespesa() {
+
+         val totalDespesas = resumo.despesa
+        with(view.resumo_card_despesa){
+            setTextColor(corDespesa)
+            text = totalDespesas.formataParaBrasileiro()
+        }
+    }
+
+    private fun adcionaTotal(){
+        val total = resumo.total
+        val cor = corPor(total)
+        with(view.resumo_card_total){
+            setTextColor(cor)
+            text = resumo.total.formataParaBrasileiro()
+        }
+
+    }
+
+    private fun corPor(total: BigDecimal): Int {
+        if (total >= BigDecimal.ZERO) {
+           return corReceita
+        }
+        return corDespesa
     }
 
 }
--- app/src/main/java/com/example/nelsonluz/financask/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/com/example/nelsonluz/financask/ui/activity/ListaTransacoesActivity.kt
@@ -1,53 +1,143 @@
 package com.example.nelsonluz.financask.ui.activity
 
+import android.app.DatePickerDialog
+import android.content.DialogInterface
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.example.nelsonluz.financask.R
+import com.example.nelsonluz.financask.extension.formataParaBrasileiro
 import com.example.nelsonluz.financask.model.Tipo
 import com.example.nelsonluz.financask.model.Transacao
 import com.example.nelsonluz.financask.ui.ResumoView
 import com.example.nelsonluz.financask.ui.adapter.ListaTransacoesAdapter
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
+import kotlinx.android.synthetic.main.form_transacao.view.*
 import java.math.BigDecimal
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ListaTransacoesActivity: AppCompatActivity() {
+
+    private val transacoes: MutableList<Transacao> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
 
-        val transacoes: List<Transacao> = transacoesDeExemplo()
+        configuraResumo()
 
-        configuraResumo(transacoes)
+        configuraLista()
 
-        configuraLista(transacoes)
+        lista_transacoes_adiciona_receita
+                .setOnClickListener {
+                    val decorView: View = window.decorView
+                    val viewCriada = LayoutInflater.from(this)
+                            .inflate(R.layout.form_transacao,
+                                    decorView as ViewGroup,
+                                    false)
+
+                    val ano = 2017
+                    val mes = 9
+                    val dia = 18
+                    val hoje = Calendar.getInstance()
+                    viewCriada.form_transacao_data.setText(hoje.formataParaBrasileiro())
+                    viewCriada.form_transacao_data
+                            .setOnClickListener{
+                                DatePickerDialog(this,
+                                        DatePickerDialog.OnDateSetListener { view, ano, mes, dia ->
+                                            val dataSelecionada = Calendar.getInstance()
+                                            dataSelecionada.set(ano,mes, dia)
+                                            viewCriada.form_transacao_data
+                                                    .setText(dataSelecionada.formataParaBrasileiro())
+                                        }
+                                        , ano, mes, dia)
+                                        .show()
+                    }
+
+                    val adapter = ArrayAdapter
+                            .createFromResource(this,
+                                    R.array.categorias_de_receita,
+                                    android.R.layout.simple_spinner_dropdown_item)
+                    viewCriada.form_transacao_categoria.adapter = adapter
+
+
+                    AlertDialog.Builder(this)
+                            .setTitle(R.string.adiciona_receita)
+                            .setView(viewCriada)
+                            .setPositiveButton("Adicionar",
+                               { dialogInterface, which ->
+                                    val valorEmTexto = viewCriada
+                                            .form_transacao_valor.text.toString()
+                                    val dataEmTexto = viewCriada
+                                            .form_transacao_data.text.toString()
+                                    val categoriaEmTexto = viewCriada
+                                            .form_transacao_categoria.selectedItem.toString()
+
+                                   val valor = try {
+                                       BigDecimal(valorEmTexto)
+                                   } catch (exception: NumberFormatException) {
+                                       Toast.makeText(this,
+                                               "Falha na Conversão de valor",
+                                               Toast.LENGTH_LONG)
+                                               .show()
+                                       BigDecimal.ZERO
+                                   }
+
+                                    val formatoBrasileiro = SimpleDateFormat("dd/MM/yyyy")
+                                    val dataConvertida: Date = formatoBrasileiro.parse(dataEmTexto)
+                                    val data = Calendar.getInstance()
+                                    data.time = dataConvertida
+
+                                    val transacaoCriada = Transacao(
+                                            tipo = Tipo.RECEITA,
+                                            valor = valor,
+                                            data = data,
+                                            categoria = categoriaEmTexto)
+
+                                    atualizaTransacoes(transacaoCriada)
+                                    lista_transacoes_adiciona_menu.close(true)
+                            })
+                            .setNegativeButton("Cancelar", null)
+                            .show()
+                }
+
     }
 
-    private fun configuraResumo(transacoes: List<Transacao>) {
+    private fun atualizaTransacoes(transacaoCriada: Transacao) {
+        transacoes.add(transacaoCriada)
+        configuraLista()
+        configuraResumo()
+    }
+
+    private fun configuraResumo() {
         val decorView: View = window.decorView
-        val resumoView = ResumoView(decorView, transacoes)
-        resumoView.adicionaReceita()
-        resumoView.adicionaDespesa()
+        val resumoView = ResumoView(this, decorView, transacoes)
+        resumoView.atualiza()
     }
 
 
-    private fun configuraLista(transacoes: List<Transacao>) {
+    private fun configuraLista() {
         lista_transacoes_listview.adapter = ListaTransacoesAdapter(transacoes, this)
     }
 
-    private fun transacoesDeExemplo(): List<Transacao> {
-        return listOf(
-                Transacao(valor = BigDecimal(20.5),
-                        tipo = Tipo.DESPESA,
-                        categoria = "Almoço de final de semana"),
-                Transacao(valor = BigDecimal(100.0),
-                        tipo = Tipo.RECEITA,
-                        categoria = "Economia"),
-                Transacao(valor = BigDecimal(200.0),
-                        tipo = Tipo.DESPESA),
-                Transacao(valor = BigDecimal(500.0),
-                        categoria = "Prêmio",
-                        tipo = Tipo.RECEITA))
-    }
+//    private fun transacoesDeExemplo(): List<Transacao> {
+//        return listOf(
+//                Transacao(valor = BigDecimal(100.0),
+//                        tipo = Tipo.DESPESA,
+//                        categoria = "Almoço de final de semana"),
+//                Transacao(valor = BigDecimal(100.0),
+//                        tipo = Tipo.RECEITA,
+//                        categoria = "Economia"),
+//                Transacao(valor = BigDecimal(100.0),
+//                        tipo = Tipo.DESPESA),
+//                Transacao(valor = BigDecimal(200.0),
+//                        categoria = "Prêmio",
+//                        tipo = Tipo.RECEITA))
+//    }
 }
